82093389e17fa4b2000730ee8b504cc3eb1a405f
sherzod <sherzod.gapirov@mail.ru>
2020-06-11 17:46:16 +0600

    Fix error in usb module.

--- src/api/internal/usb.ts
+++ src/api/internal/usb.ts
@@ -1,3 +1,4 @@
+const os = require("os");
 const usb = require("usb");
 
 const printer = {
@@ -83,15 +84,15 @@ export function initUsb() {
         iface.setAltSetting(iface.altSetting, () => {
           // http://libusb.sourceforge.net/api-1.0/group__dev.html#gab14d11ed6eac7519bb94795659d2c971
           // libusb_kernel_driver_active / libusb_attach_kernel_driver / libusb_detach_kernel_driver : "This functionality is not available on Windows."
-          if ("win32" !== os.platform()) {
-            if(iface.isKernelDriverActive()) {
-              try {
+          try {
+            if ("win32" !== os.platform()) {
+              if(iface.isKernelDriverActive()) {
                 iface.detachKernelDriver();
-              } catch(e) {
-                console.error("[ERROR] Could not detatch kernel driver: %s", e)
-                reject(`[ERROR] Could not detatch kernel driver: ${e}`);
               }
             }
+          } catch(e) {
+            console.error("[ERROR] Could not detatch kernel driver: %s", e)
+            reject(`[ERROR] Could not detatch kernel driver: ${e}`);
           }
           iface.claim(); // must be called before using any endpoints of this interface.
           iface.endpoints.forEach(endpoint => {
